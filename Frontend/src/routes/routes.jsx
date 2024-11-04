99fb5b05f6f11e7af979dc3bda13afc37a744b61
Suhas-30 <3suhashs@gmail.com>
2024-11-04 12:50:16 +0530

    SHCMS Project

--- Frontend/src/routes/routes.jsx
+++ Frontend/src/routes/routes.jsx
@@ -7,122 +7,95 @@ import React from "react";
 import CustomerRegistration from "../pages/customer/CustomerRegistration";
 import CoustomerLogin from "../pages/customer/CustomerLogin";
 import PrivateRoute from "./PrivateRoutes";
-<<<<<<< HEAD
 import Payment from "../pages/payment/Payment";
 import PurchaseDetails from "../pages/payment/PurchaseDetails";
 import KnowDetails from "../pages/customer/KnowDetails";
 import AdminAuthen from "../Admin/AdminAuthen";
 import AdminNav from "../Admin/AdminNav";
 import SalesHistory from "../Admin/SalesHistory";
-=======
 
->>>>>>> 7a9e8950d969671ba9843492bb92583c860f6b9e
 export const routes = createBrowserRouter([
     {
         path: '/', element: (
             <div>
-                <Navigation></Navigation>
-                <Home></Home>
+                <Navigation />
+                <Home />
             </div>
         )
     },
     {
         path: '/previous-owner-form', element: (
-<<<<<<< HEAD
-                <div>
-                <AdminNav></AdminNav>
-                <PreviousOwnerForm></PreviousOwnerForm>
-                </div>
-=======
             <PrivateRoute>
                 <div>
-                <Navigation></Navigation>
-                <PreviousOwnerForm></PreviousOwnerForm>
+                    <Navigation />
+                    <PreviousOwnerForm />
                 </div>
             </PrivateRoute>
->>>>>>> 7a9e8950d969671ba9843492bb92583c860f6b9e
         )
     },
     {
         path: '/add-car-form', element: (
-<<<<<<< HEAD
-                <div>
-                <AdminNav></AdminNav>
-                <AddCars></AddCars>
-                </div>
-
-=======
             <PrivateRoute>
                 <div>
-                <Navigation></Navigation>
-                <AddCars></AddCars>
+                    <Navigation />
+                    <AddCars />
                 </div>
             </PrivateRoute>
->>>>>>> 7a9e8950d969671ba9843492bb92583c860f6b9e
         )
     },
-
     {
         path: '/customer-reg', element: (
             <div>
-                <Navigation></Navigation>
-                <CustomerRegistration></CustomerRegistration>
+                <Navigation />
+                <CustomerRegistration />
             </div>
         )
     },
     {
-        path: '/customer-login', element:(
+        path: '/customer-login', element: (
             <div>
-                <Navigation></Navigation>
-                <CoustomerLogin></CoustomerLogin>
+                <Navigation />
+                <CoustomerLogin />
             </div>
         )
-<<<<<<< HEAD
     },
     {
         path:'/payment',
         element: (
-            <div>
-                <PrivateRoute>
-                <Payment></Payment>
-                </PrivateRoute>
-            </div>
+            <PrivateRoute>
+                <Payment />
+            </PrivateRoute>
         )
     },
     {
         path:'/purchase-details',
         element: (
-            <div>
-                <PrivateRoute>
-                <PurchaseDetails></PurchaseDetails>
-                </PrivateRoute>
-            </div>
+            <PrivateRoute>
+                <PurchaseDetails />
+            </PrivateRoute>
         )
     },
     {
         path: '/know-details',
-        element:(
+        element: (
+            <PrivateRoute>
+                <KnowDetails />
+            </PrivateRoute>
+        )
+    },
+    {
+        path: '/admin', element: (
             <div>
-                <PrivateRoute>
-                <KnowDetails></KnowDetails>
-                </PrivateRoute>
+                <AdminAuthen />
             </div>
         )
     },
     {
-        path: '/admin', element:(
+        path: '/sales', element: (
             <div>
-                <AdminAuthen></AdminAuthen>
+                <AdminNav />
+                <SalesHistory />
             </div>
         )
-    },{
-        path: '/sales', element:(
-            <div>
-                <AdminNav></AdminNav>
-                <SalesHistory></SalesHistory>
-            </div>
-        )
-=======
->>>>>>> 7a9e8950d969671ba9843492bb92583c860f6b9e
     }
-])
+]);
